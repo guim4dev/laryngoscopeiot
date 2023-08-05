c904e9d00483f3ac626fa0df77ea1c6e3935cf78
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2023-08-04 22:09:31 -0300

    add i18n, remove vue base code, add title and logo

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,18 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ t("welcome") }}</h1>
 </template>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+const i18n = useI18n();
+const { t } = i18n;
+
+onMounted(() => {
+  const userLanguage = navigator.language;
+  i18n.locale.value = userLanguage;
+});
+</script>
 
 <style scoped>
 .logo {
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createI18n, type I18nOptions } from "vue-i18n";
+import * as en from "../locales/en.json";
+import * as ptBr from "../locales/pt-BR.json";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const options: I18nOptions = {
+  legacy: false,
+  locale: "en",
+  fallbackLocale: "en",
+  messages: {
+    en,
+    "pt-BR": ptBr,
+  },
+};
+
+const i18n = createI18n<false, typeof options>(options);
+const app = createApp(App);
+
+app.use(i18n);
+app.mount("#app");
