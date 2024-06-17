8e36e77d69e91f1a8bcd9968f02d221be185f977
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2024-06-17 19:14:06 -0300

    trying to fix camera D:

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -2,17 +2,18 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 // Libs Used for Camera Streaming V1
-// #include "esp_camera.h"
-// #include "esp_timer.h"
-// #include "img_converters.h"
-// #include "fb_gfx.h"
+#include "esp_camera.h"
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "fb_gfx.h"
 #include "soc/soc.h"          //disable brownout problems
 #include "soc/rtc_cntl_reg.h" //disable brownout problems
-// #include "esp_http_server.h"
 
 // Libs for async web server
+// namespace async_server {
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+// }
 
 #define CAMERA_MODEL_AI_THINKER
 
@@ -47,96 +48,106 @@ const char *password = "senhadopai";
 // TODO: current implementation sends the whole image every time. It would be better to send only the changed parts.
 // we should also consider sending a lower resolution image to save bandwidth
 // and using HLS streaming for better performance on the client side
-// static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-// static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-// static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
-// httpd_handle_t stream_httpd = NULL;
-// static esp_err_t stream_handler(httpd_req_t *req)
+static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+static AsyncWebServerRequest *cameraRequest = NULL;
+
+static void camera_stream_handler(void * parameter)
+{
+  if (!cameraRequest) {
+    return;
+  }
+
+  camera_fb_t *fb = NULL;
+  size_t _jpg_buf_len = 0;
+  uint8_t *_jpg_buf = NULL;
+  char *part_buf[64];
+
+  fb = esp_camera_fb_get();
+  if (!fb)
+  {
+    Serial.println("Camera capture failed");
+    cameraRequest->send(500, "text/plain", "Camera capture failed");
+    return;
+  }
+  else
+  {
+    if (fb->width > 400)
+    {
+      if (fb->format != PIXFORMAT_JPEG)
+      {
+        bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+        esp_camera_fb_return(fb);
+        fb = NULL;
+        if (!jpeg_converted)
+        {
+          Serial.println("JPEG compression failed");
+          cameraRequest->send(500, "text/plain", "Camera capture failed");
+        }
+      }
+      else
+      {
+        _jpg_buf_len = fb->len;
+        _jpg_buf = fb->buf;
+      }
+    }
+  }
+  Serial.println("JPG buffer length: " + String(_jpg_buf_len) + " bytes");
+  AsyncWebServerResponse *response = cameraRequest->beginResponse_P(200, "image/jpeg", _jpg_buf, _jpg_buf_len);
+  cameraRequest->send(response);
+  cameraRequest = NULL;
+  Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+
+  // delete task
+  vTaskDelete(NULL);
+
+  // while (true)
+  // {
+  //   Serial.print("A tarefa está rodando no seguinte núcleo: ");
+  //   Serial.println(xPortGetCoreID());
+  //   fb = esp_camera_fb_get();
+  //   if (!fb)
+  //   {
+  //     Serial.println("Camera capture failed");
+  //     res = ESP_FAIL;
+  //   }
+  //   else
+  //   {
+  //     if (fb->width > 400)
+  //     {
+  //       if (fb->format != PIXFORMAT_JPEG)
+  //       {
+  //         bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+  //         esp_camera_fb_return(fb);
+  //         fb = NULL;
+  //         if (!jpeg_converted)
+  //         {
+  //           Serial.println("JPEG compression failed");
+  //           res = ESP_FAIL;
+  //         }
+  //       }
+  //       else
+  //       {
+  //         _jpg_buf_len = fb->len;
+  //         _jpg_buf = fb->buf;
+  //       }
+  //     }
+  //   }
+  //   Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+  // }
+  // return res;
+}
+
+// void startCameraServer(void * parameter)
 // {
-//   camera_fb_t *fb = NULL;
-//   esp_err_t res = ESP_OK;
-//   size_t _jpg_buf_len = 0;
-//   uint8_t *_jpg_buf = NULL;
-//   char *part_buf[64];
+//   esp_http::httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+//   config.server_port = 81;
 
-//   res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-//   if (res != ESP_OK)
-//   {
-//     return res;
-//   }
-
-//   while (true)
-//   {
-//     fb = esp_camera_fb_get();
-//     if (!fb)
-//     {
-//       Serial.println("Camera capture failed");
-//       res = ESP_FAIL;
-//     }
-//     else
-//     {
-//       if (fb->width > 400)
-//       {
-//         if (fb->format != PIXFORMAT_JPEG)
-//         {
-//           bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-//           esp_camera_fb_return(fb);
-//           fb = NULL;
-//           if (!jpeg_converted)
-//           {
-//             Serial.println("JPEG compression failed");
-//             res = ESP_FAIL;
-//           }
-//         }
-//         else
-//         {
-//           _jpg_buf_len = fb->len;
-//           _jpg_buf = fb->buf;
-//         }
-//       }
-//     }
-//     if (res == ESP_OK)
-//     {
-//       size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
-//       res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-//     }
-//     if (res == ESP_OK)
-//     {
-//       res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-//     }
-//     if (res == ESP_OK)
-//     {
-//       res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-//     }
-//     if (fb)
-//     {
-//       esp_camera_fb_return(fb);
-//       fb = NULL;
-//       _jpg_buf = NULL;
-//     }
-//     else if (_jpg_buf)
-//     {
-//       free(_jpg_buf);
-//       _jpg_buf = NULL;
-//     }
-//     if (res != ESP_OK)
-//     {
-//       break;
-//     }
-//     // Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
-//   }
-//   return res;
-// }
-
-// void startCameraServer()
-// {
-//   httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-//   config.server_port = 80;
-
-//   httpd_uri_t index_uri = {
-//       .uri = "/camera_stream",
-//       .method = HTTP_GET,
-//       .handler = stream_handler,
+//   esp_http::httpd_uri_t index_uri = {
+//       .uri = "/camera",
+//       .method = esp_http::HTTP_GET,
+//       .handler = camera_stream_handler,
 //       .user_ctx = NULL};
 
 //   Serial.printf("Starting web server on port: '%d'\n", config.server_port);
@@ -146,62 +157,66 @@ const char *password = "senhadopai";
 //   }
 // }
 
-// void setupCamera()
-// {
-//   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
+bool cameraSuccesfullyInitialized = false;
 
-//   Serial.begin(115200);
-//   camera_config_t config;
-//   config.ledc_channel = LEDC_CHANNEL_0;
-//   config.ledc_timer = LEDC_TIMER_0;
-//   config.pin_d0 = Y2_GPIO_NUM;
-//   config.pin_d1 = Y3_GPIO_NUM;
-//   config.pin_d2 = Y4_GPIO_NUM;
-//   config.pin_d3 = Y5_GPIO_NUM;
-//   config.pin_d4 = Y6_GPIO_NUM;
-//   config.pin_d5 = Y7_GPIO_NUM;
-//   config.pin_d6 = Y8_GPIO_NUM;
-//   config.pin_d7 = Y9_GPIO_NUM;
-//   config.pin_xclk = XCLK_GPIO_NUM;
-//   config.pin_pclk = PCLK_GPIO_NUM;
-//   config.pin_vsync = VSYNC_GPIO_NUM;
-//   config.pin_href = HREF_GPIO_NUM;
-//   config.pin_sscb_sda = SIOD_GPIO_NUM;
-//   config.pin_sscb_scl = SIOC_GPIO_NUM;
-//   config.pin_pwdn = PWDN_GPIO_NUM;
-//   config.pin_reset = RESET_GPIO_NUM;
-//   config.xclk_freq_hz = 20000000;
-//   config.pixel_format = PIXFORMAT_JPEG;
+void setupCamera()
+{
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
 
-//   if (psramFound())
-//   {
-//   config.frame_size = FRAMESIZE_XGA;
-//   config.jpeg_quality = 4;
-//   config.fb_count = 24;
-//   }
-//   else
-//   {
-//     config.frame_size = FRAMESIZE_SVGA;
-//     config.jpeg_quality = 12;
-//     config.fb_count = 1;
-//   }
+  if (psramFound())
+  {
+    Serial.println("PSRAM found");
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 20;
+    config.fb_count = 1;
+  }
+  else
+  {
+    Serial.println("PSRAM not found");
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 20;
+    config.fb_count = 1;
+  }
 
-//   // Camera init
-//   esp_err_t err = esp_camera_init(&config);
-//   if (err != ESP_OK)
-//   {
-//     Serial.printf("Camera init failed with error 0x%x", err);
-//     return;
-//   }
-//   Serial.println("");
-//   Serial.println("WiFi connected");
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
 
-//   Serial.print("Camera Stream Ready! Go to: http://");
-//   Serial.println(WiFi.localIP());
-
-//   // Start streaming web server
-//   startCameraServer();
-// }
+  // xTaskCreatePinnedToCore(
+  //     startCameraServer, /* Function to implement the task */
+  //     "camera_server",  /* Name of the task */
+  //     20000,            /* Stack size in words */
+  //     NULL,             /* Task input parameter */
+  //     0,                /* Priority of the task */
+  //     NULL,             /* Task handle. */
+  //     1                 /* Core where the task should run */
+  // );
+  cameraSuccesfullyInitialized = true;
+}
 
 void setupTeethButton()
 {
@@ -287,12 +302,6 @@ int teethSensor = 0;
 void captureTeethSensorValue()
 {
   teethSensor = !digitalRead(TEETH_BUTTON_PIN); // button is connected to GND - so when pressed, it will read LOW.
-  // if (teethSensor == HIGH)
-  // {
-  //   Serial.println("Button pressed. STOP TOUCHING THE TEETH!!!!!");
-  // } else {
-  //   Serial.println("Button not pressed. Youre doing fine :D.");
-  // }
 }
 
 struct ForceSensorsCapture {
@@ -316,22 +325,13 @@ void captureForceSensorsValues()
   sensorsCapture.secondaryForce = secondaryForceValue;
 }
 
+// TODO: maybe remove this
 String processor(const String& var) {
-  // if(var == "TEMPERATURE"){
-  //   return String(temperature);
-  // }
-  // else if(var == "HUMIDITY"){
-  //   return String(humidity);
-  // }
-  // else if(var == "FORCE"){
-  //   return String(force);
-  // }
   return String();
 }
 
 AsyncWebServer server(80);
 AsyncEventSource sensorEvents("/sensors");
-AsyncEventSource cameraEvents("/camera");
 
 const char alive_json[] PROGMEM = R"rawliteral(
 {
@@ -340,17 +340,29 @@ const char alive_json[] PROGMEM = R"rawliteral(
 
 unsigned long last_time_sensors = 0;
 unsigned long sensors_delay = 500; // 0.5 seconds delay
-unsigned long last_time_camera = 0;
-unsigned long camera_update_delay =  41.6; // 24fps
-// unsigned long camera_update_delay =  33.3; // 30fps
-// unsigned long camera_update_delay =  16.6; // 60fps
+
+bool connectedToSensors = false;
+bool connectedToCamera = false;
 
 void setupAsyncWebServer () {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "application/json", alive_json, processor);
   });
 
-  // setup cors
+  // TODO: fix camera
+  // server.on("/camera", HTTP_GET, [](AsyncWebServerRequest *request){
+  //   cameraRequest = request;
+  //   xTaskCreate(
+  //     camera_stream_handler,  
+  //     "camera_stream_task",
+  //     10000,
+  //     NULL,
+  //     1,
+  //     NULL
+  //   );
+  // });
+
+  // setup cors and OPTIONS support
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
   server.onNotFound([](AsyncWebServerRequest *request) {
     if (request->method() == HTTP_OPTIONS) {
@@ -366,18 +378,10 @@ void setupAsyncWebServer () {
     }
 
     client->send("connected", NULL, millis(), 10000);
-  });
-
-  cameraEvents.onConnect([](AsyncEventSourceClient *client){
-    if(client->lastId()){
-      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
-    }
-
-    client->send("connected", NULL, millis(), 10000);
+    connectedToSensors = true;
   });
 
   server.addHandler(&sensorEvents);
-  server.addHandler(&cameraEvents);
   server.begin();
 }
 
@@ -394,32 +398,24 @@ void setup()
   
   setupWifi();
   setupTeethButton();
-  setupAsyncWebServer();
+  // TODO: FIX CAMERA
   // setupCamera();
+  setupAsyncWebServer();
   Serial.println("Setup done");
   Serial.flush();
 }
 
-
-// TODO: try to split these two functions into two separate loops -> one for sensors and one for camera
+// TODO: move sensors to separate file + use RTOsTasks
 void loop()
 {
   int loop_start = millis();
   int time_diff_sensors = loop_start - last_time_sensors;
-  if (time_diff_sensors > sensors_delay) {
+  if (time_diff_sensors > sensors_delay && connectedToSensors) {
     captureTeethSensorValue();
     captureForceSensorsValues();
     String sensorData = "{\"primaryForce\": " + String(sensorsCapture.primaryForce) + ", \"secondaryForce\": " + String(sensorsCapture.secondaryForce) + ", \"teethPressed\": " + String(teethSensor) + " }";
     sensorEvents.send(sensorData.c_str(), "sensorData", millis());
-    // Serial.println("Sending sensor data to client: " + sensorData);
     Serial.flush();
     last_time_sensors = loop_start;
-  }
-
-  int time_diff_camera = loop_start - last_time_camera;
-  if (time_diff_camera > camera_update_delay) {
-    // captureCameraImage();
-    // TODO: implement capturing image and sending it to the client
-    last_time_camera = loop_start;
   }
 }
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -88,6 +88,7 @@ onMounted(() => {
 
 .loading {
   height: 100%;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
